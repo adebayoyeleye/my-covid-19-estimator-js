3e5234e2fcacdd28e33b7f6391247299c386ad3e
Adebayo Oyeleye <adebayo.oyeleye@gmail.com>
2020-04-13 06:39:41 +0100

    ok

--- src/estimator.js
+++ src/estimator.js
@@ -22,7 +22,7 @@ const covid19ImpactEstimator = (data) => {
                                         * (2 ** getInfectionFactor(normalisedTimeToElapse));
   impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.15);
   impact.hospitalBedsByRequestedTime = Math.floor(data.totalHospitalBeds * 0.35
-                                                - (impact.infectionsByRequestedTime * 0.15));
+                                                - (impact.infectionsByRequestedTime * 0.15) - 1);
   impact.casesForICUByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.05);
   impact.casesForVentilatorsByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.02);
   impact.dollarsInFlight = Math.floor(
@@ -39,7 +39,7 @@ const covid19ImpactEstimator = (data) => {
     severeImpact.infectionsByRequestedTime * 0.15
   );
   severeImpact.hospitalBedsByRequestedTime = Math.floor(data.totalHospitalBeds * 0.35
-                                                - (severeImpact.infectionsByRequestedTime * 0.15));
+                                            - (severeImpact.infectionsByRequestedTime * 0.15) - 1);
   severeImpact.casesForICUByRequestedTime = Math.floor(
     severeImpact.infectionsByRequestedTime * 0.05
   );

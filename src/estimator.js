4618b398bb09aacc3337d49b9cd79c1574d5db4a
Adebayo Oyeleye <adebayo.oyeleye@gmail.com>
2020-04-13 08:02:21 +0100

    some refactoring&fixed math.floor 4 -ive num

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-const normaliseTimeToElapse = (periodType, timeToElapse) => {
+const getNormalisedTimeToElapse = (periodType, timeToElapse) => {
   let normalisedTimeToElapse;
   if (periodType === 'weeks') {
     normalisedTimeToElapse = timeToElapse * 7;
@@ -13,44 +13,87 @@ const normaliseTimeToElapse = (periodType, timeToElapse) => {
 
 const getInfectionFactor = (timeToElapse) => Math.floor(timeToElapse / 3);
 
+const getiInfectionsByRequestedTime = (data) => this.currentlyInfected
+        * (2 ** getInfectionFactor(getNormalisedTimeToElapse(data.periodType, data.timeToElapse)));
+
+const getSevereCasesByRequestedTime = () => Math.floor(this.infectionsByRequestedTime * 0.15);
+
+const getHospitalBedsByRequestedTime = (data) => {
+  const tempHospitalBeds = (data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime;
+  return (tempHospitalBeds >= 0 || -1) * Math.floor(Math.abs(tempHospitalBeds));
+};
+
+const getCasesForICUByRequestedTime = () => Math.floor(this.infectionsByRequestedTime * 0.05);
+
+const getCasesForVentilatorsByRequestedTime = () => Math.floor(
+  this.infectionsByRequestedTime * 0.02
+);
+
+const getDollarsInFlight = (data) => {
+  Math.floor(
+    (this.infectionsByRequestedTime
+            * data.avgDailyIncomePopulation
+            * data.avgDailyIncomeInUSD) / 30
+  );
+};
+
 const covid19ImpactEstimator = (data) => {
-  const normalisedTimeToElapse = normaliseTimeToElapse(data.periodType, data.timeToElapse);
+//   const normalisedTimeToElapse = getNormalisedTimeToElapse(data.periodType, data.timeToElapse);
 
   const impact = {};
   impact.currentlyInfected = data.reportedCases * 10;
-  impact.infectionsByRequestedTime = impact.currentlyInfected
-                                        * (2 ** getInfectionFactor(normalisedTimeToElapse));
-  impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.15);
-  impact.hospitalBedsByRequestedTime = Math.floor((data.totalHospitalBeds * 0.35
-                                                - impact.infectionsByRequestedTime * 0.15) - 1);
-  impact.casesForICUByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.05);
-  impact.casesForVentilatorsByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.02);
-  impact.dollarsInFlight = Math.floor(
-    (impact.infectionsByRequestedTime
-        * data.avgDailyIncomePopulation
-        * data.avgDailyIncomeInUSD) / 30
-  );
+  //   impact.infectionsByRequestedTime = impact.currentlyInfected
+  //                                         * (2 ** getInfectionFactor(normalisedTimeToElapse));
+  impact.infectionsByRequestedTime = getiInfectionsByRequestedTime(data);
+  //   impact.severeCasesByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.15);
+  impact.severeCasesByRequestedTime = getSevereCasesByRequestedTime();
+  //   impact.tempHospitalBeds = (data.totalHospitalBeds * 0.35)
+  //                                             - impact.severeCasesByRequestedTime;
+  //   impact.hospitalBedsByRequestedTime = (impact.tempHospitalBeds >= 0 || -1)
+  //                                     * Math.floor(Math.abs(impact.tempHospitalBeds));
+  impact.hospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(data);
+  //   impact.casesForICUByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.05);
+  impact.casesForICUByRequestedTime = getCasesForICUByRequestedTime();
+  // impact.casesForVentilatorsByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.02)
+  impact.casesForVentilatorsByRequestedTime = getCasesForVentilatorsByRequestedTime();
+  //   impact.dollarsInFlight = Math.floor(
+  //     (impact.infectionsByRequestedTime
+  //         * data.avgDailyIncomePopulation
+  //         * data.avgDailyIncomeInUSD) / 30
+  //   );
+  impact.dollarsInFlight = getDollarsInFlight(data);
 
   const severeImpact = {};
   severeImpact.currentlyInfected = data.reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-                                                * (2 ** getInfectionFactor(normalisedTimeToElapse));
-  severeImpact.severeCasesByRequestedTime = Math.floor(
-    severeImpact.infectionsByRequestedTime * 0.15
-  );
-  severeImpact.hospitalBedsByRequestedTime = Math.floor((data.totalHospitalBeds * 0.35
-                                            - severeImpact.infectionsByRequestedTime * 0.15) - 1);
-  severeImpact.casesForICUByRequestedTime = Math.floor(
-    severeImpact.infectionsByRequestedTime * 0.05
-  );
-  severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
-    severeImpact.infectionsByRequestedTime * 0.02
-  );
-  severeImpact.dollarsInFlight = Math.floor(
-    (severeImpact.infectionsByRequestedTime
-        * data.avgDailyIncomePopulation
-        * data.avgDailyIncomeInUSD) / 30
-  );
+  //   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
+  // eslint-disable-next-line max-len
+  //                                                 * (2 ** getInfectionFactor(normalisedTimeToElapse));
+  severeImpact.infectionsByRequestedTime = getiInfectionsByRequestedTime(data);
+  //   severeImpact.severeCasesByRequestedTime = Math.floor(
+  //     severeImpact.infectionsByRequestedTime * 0.15
+  //   );
+  severeImpact.severeCasesByRequestedTime = getSevereCasesByRequestedTime();
+  //   severeImpact.tempHospitalBeds = (data.totalHospitalBeds * 0.35)
+  //                                     - severeImpact.severeCasesByRequestedTime;
+  //   severeImpact.hospitalBedsByRequestedTime = (severeImpact.tempHospitalBeds >= 0 || -1)
+  //                                          * Math.floor(Math.abs(severeImpact.tempHospitalBeds));
+  severeImpact.hospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(data);
+
+  //   severeImpact.casesForICUByRequestedTime = Math.floor(
+  //     severeImpact.infectionsByRequestedTime * 0.05
+  //   );
+  severeImpact.casesForICUByRequestedTime = getCasesForICUByRequestedTime();
+  //   severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
+  //     severeImpact.infectionsByRequestedTime * 0.02
+  //   );
+  severeImpact.casesForVentilatorsByRequestedTime = getCasesForVentilatorsByRequestedTime();
+  //   severeImpact.dollarsInFlight = Math.floor(
+  //     (severeImpact.infectionsByRequestedTime
+  //         * data.avgDailyIncomePopulation
+  //         * data.avgDailyIncomeInUSD) / 30
+  //   );
+  severeImpact.dollarsInFlight = getDollarsInFlight(data);
+
 
   const output = {
     data, // the input data you got

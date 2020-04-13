6614a07cf55e0b0ad000b081dd545af11ced4cea
Adebayo Oyeleye <adebayo.oyeleye@gmail.com>
2020-04-13 04:33:04 +0100

    Fixed Eslinting

--- src/estimator.js
+++ src/estimator.js
@@ -1,31 +1,41 @@
-const covid19ImpactEstimator = (data) => {
-
-    let normalisedTimeToElapse = normaliseTimeToElapse(data.periodType, data.timeToElapse);
-
-    let impact = {}
-    impact.currenlyInfected = data.reportedCases * 10;
-    impact.infectionsByRequestedTime = impact.currenlyInfected * (2 ** getInfectionFactor(normalisedTimeToElapse));
-
-    let severeImpact = {}
-    severeImpact.currenlyInfected = data.reportedCases * 50;
-    severeImpact.infectionsByRequestedTime = severeImpact.currenlyInfected * (2 ** getInfectionFactor(normalisedTimeToElapse));
-
-
-    let output = {
-        data: data, // the input data you got
-        impact: impact, // your best case estimation
-        severeImpact: severeImpact  //severe case estimation
-    };
-
-    return output;
-
-};
-
+/* eslint-disable linebreak-style */
 const normaliseTimeToElapse = (periodType, timeToElapse) => {
-    return (periodType==="week"?timeToElapse*7:(periodType==="months"?timeToElapse*30:timeToElapse));
+  let normalisedTimeToElapse;
+  if (periodType === 'week') {
+    normalisedTimeToElapse = timeToElapse * 7;
+  } else if (periodType === 'months') {
+    normalisedTimeToElapse = timeToElapse * 30;
+  } else {
+    normalisedTimeToElapse = timeToElapse;
+  }
+  return normalisedTimeToElapse;
 };
 
-const getInfectionFactor = (timeToElapse) => Math.floor(timeToElapse/3);
+const getInfectionFactor = (timeToElapse) => Math.floor(timeToElapse / 3);
+
+const covid19ImpactEstimator = (data) => {
+  const normalisedTimeToElapse = normaliseTimeToElapse(data.periodType, data.timeToElapse);
+
+  const impact = {};
+  impact.currenlyInfected = data.reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currenlyInfected
+                                        * (2 ** getInfectionFactor(normalisedTimeToElapse));
+
+  const severeImpact = {};
+  severeImpact.currenlyInfected = data.reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currenlyInfected
+                                                * (2 ** getInfectionFactor(normalisedTimeToElapse));
+
+
+  const output = {
+    data, // the input data you got
+    impact, // your best case estimation
+    severeImpact // severe case estimation
+  };
+
+  return output;
+};
+
 
 export default covid19ImpactEstimator;
 
